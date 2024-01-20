79b2881e4994d05c136702c38cfa8218bf585ec4
ricardocisco <ricardopenga70@gmail.com>
2024-01-19 23:47:18 -0300

    update

--- src/App.js
+++ src/App.js
@@ -1,24 +1,53 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import api from "../src/services/api"
+
 
 function App() {
+
+  const [value, setValue] = useState('');
+
+  const [cep, setCep] = useState({});
+
+  async function handleSearch(){
+    if( value === ""){
+      alert("Campo Vazio!")
+      return;
+    }
+
+    try{
+      const response = await api.get(`${value}/json`);
+      setCep(response.data);
+      setValue("");
+    }catch{
+      alert("Erro ao buscar!")
+      setValue("")
+    }
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+  <div className="wrapper">
+    <h1 className="box-title">Busca Cep</h1>
+    <div className="box">
+      <input 
+      type="text"
+      placeholder="Insira seu cep"
+      value={value}
+      onChange={(e) => setValue(e.target.value)}
+      className="box-input"
+      />
+      <button className="box-btn" onClick={handleSearch}>Buscar</button>
     </div>
+
+    {Object.keys(cep).length > 0 && (
+      <main className="box-main">
+        <p>CEP: {cep.cep}</p>
+        <p>Rua: {cep.logradouro}</p>
+        <p>Bairro: {cep.bairro}</p>
+        <p>Cidade: {cep.localidade}</p>
+        <p>Estado: {cep.uf}</p>
+      </main>
+    )}
+  </div>
   );
 }
 
